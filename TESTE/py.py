c7e8bed72c0eab9aa1a900fde2ea78c32ff9aed1
Gabriel <oliveira.barboza84@gmail.com>
2025-08-25 20:16:37 -0300

    refactor: Remove unnecessary comments for improved code clarity

--- TESTE/py.py
+++ TESTE/py.py
@@ -1,17 +1,14 @@
 import re
 import unicodedata
 
-# Abre o arquivo
+
 with open(r'c:\Users\super\projetos\letrAZ\src\palavras.txt', 'r', encoding='utf-8') as file:
     texto = file.read()
 
-# Pega todas as palavras
 palavras = re.findall(r'\b\w+\b', texto)
 
-# Filtra palavras com mais de 3 letras
 palavras_filtradas = [palavra for palavra in palavras if len(palavra) == 5]
 
-# Remove duplicatas
 palavras_unicas = list(set(palavras_filtradas))
 
 def remover_acentos(palavra):
@@ -22,17 +19,15 @@ def remover_acentos(palavra):
 
 palavras_unicas = [remover_acentos(p) for p in palavras_unicas]
 
-# Ordena as palavras
 palavras_unicas.sort()
-# Exibe as palavras como uma lista
+
 with open('lista_com_aspas_virgula.txt', 'w', encoding='utf-8') as f:
     f.write('[\n')
     for palavra in palavras_unicas:
         f.write(f"'{palavra}',\n")
     f.write(']\n')
 
-# Remove palavras que começam com 'a' ou 'A'
-# Salva em um novo arquivo
+
 with open('lista_filtrada.txt', 'w', encoding='utf-8') as file:
     for palavra in palavras_unicas:
         file.write(palavra + '\n')
